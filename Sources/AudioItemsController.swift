6d7234628ee8aa60c77cc5bb08eb9300d12b15ea
Alexander Shvets <alexander.shvets@gmail.com>
2017-03-25 17:25:41 -0400

    update audio player

--- Sources/AudioItemsController.swift
+++ Sources/AudioItemsController.swift
@@ -52,7 +52,7 @@ class AudioItemsController: BaseTableViewController {
   override open func navigate(from view: UITableViewCell) {
     //self.navigationController?.setNavigationBarHidden(true, animated: false)
 
-    performSegue(withIdentifier: AudioPlayerController.SegueIdentifier, sender: view)
+    performSegue(withIdentifier: AudioPlayerController3.SegueIdentifier, sender: view)
 
 //    let mediaItem = getItem(for: view)
 //
@@ -121,7 +121,7 @@ class AudioItemsController: BaseTableViewController {
 //          }
 //        }
 
-      performSegue(withIdentifier: AudioPlayerController.SegueIdentifier, sender: gesture.view)
+      performSegue(withIdentifier: AudioPlayerController3.SegueIdentifier, sender: gesture.view)
     }
   }
 #endif
@@ -131,11 +131,12 @@ class AudioItemsController: BaseTableViewController {
   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
     if let identifier = segue.identifier {
       switch identifier {
-        case AudioPlayerController.SegueIdentifier:
-          if let destination = segue.destination as? AudioPlayerController {
+        case AudioPlayerController3.SegueIdentifier:
+          if let destination = segue.destination as? AudioPlayerController3 {
 
             destination.mediaItem = getItem(for: sender as! UITableViewCell)
             destination.items = items
+            destination.trackID = 1
           }
 
         default: break
