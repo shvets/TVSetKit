1363baf7a9578b01e306eed25d6df04385dc6355
Alexander Shvets <alexander.shvets@gmail.com>
2017-10-31 10:15:37 -0400

    add reusable protocols

--- Sources/ios/ReusableController.swift
+++ Sources/ios/ReusableController.swift
@@ -1,7 +1,6 @@
 protocol ReusableController: class {}
 
 extension ReusableController where Self: UIViewController {
-  
   static var reuseIdentifier: String {
     return String(describing: self)
   }
