17f06c3def1648e91bd6d9b3b7a6d1b28bba03b9
Alexander Shvets <alexander.shvets@gmail.com>
2018-11-04 20:22:19 -0500

    bump version

--- Sources/MenuItem.swift
+++ Sources/MenuItem.swift
@@ -2,6 +2,11 @@ import Foundation
 import UIKit
 
 public struct MenuItem {
-  public let name: String?
-  public let imageName: String?
+  public var name: String?
+  public var imageName: String?
+
+  public init(name: String? = nil, imageName: String? = nil) {
+    self.name = name
+    self.imageName = imageName
+  }
 }
